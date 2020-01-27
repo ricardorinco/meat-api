d3a9929ea96063215f90670a6f578332921b96df
Ricardo Rinco <ricardorinco@procobservicos.com.br>
2020-01-27 08:37:21 -0300

    Os objetos de Request, Response e Next

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import * as restify from 'restify';
+import * as restify from "restify";
 
 const port = 3000;
 const server = restify.createServer({
@@ -6,10 +6,37 @@ const server = restify.createServer({
     version: "0.1.0"
 });
 
-server.get('/hello', (req, resp, next) => {
-    resp.json({ message: 'hello' });
-    return next();
-});
+server.use(restify.plugins.queryParser());
+
+server.get("/info", [
+    (req, resp, next) => {
+        if (req.userAgent() && req.userAgent().includes('MSIE 7.0')) {
+            // resp.status(400);
+            // resp.json({
+            //     message: 'Please, update your brownser'
+            // })
+
+            let error:any = new Error();
+            error.statusCode = 400;
+            error.message = 'Please, update your brownser'
+
+            return next(error);
+        }
+
+        return next();
+    },
+    (req, resp, next) => {
+        resp.json({
+            browser: req.userAgent(),
+            method: req.method,
+            ur: req.href(),
+            path: req.path(),
+            query: req.query
+        });
+
+        return next();
+    }
+]);
 
 server.listen(port, () => {
     console.log(`API is running on http://localhost:${port}`);
