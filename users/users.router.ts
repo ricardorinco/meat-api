5e1555285b05c881a284fba49f16287ae1887da8
Ricardo Rinco <ricardorinco@procobservicos.com.br>
2020-01-30 06:58:38 -0300

    Mapeando o Schema de um Documento com Mongoose

--- users/users.router.ts
+++ users/users.router.ts
@@ -6,8 +6,9 @@ class UsersRouter extends Router {
 
     applyRoutes(application: restify.Server) {
         application.get('/users', (req, resp, next) => {
-            User.findAll().then(users => {
+            User.find().then(users => {
                 resp.json(users);
+                return next();
             })
         });
 
