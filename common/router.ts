ca19aa3cddfe744b506e1a5606f797afe6fb01fd
Ricardo Rinco <ricardo.rinco@hotmail.com>
2020-02-01 11:35:24 -0300

    Refactoring para Reuso do Router Render

--- common/router.ts
+++ common/router.ts
@@ -1,7 +1,22 @@
 import * as restify from 'restify';
+import { EventEmitter } from 'events';
 
-export abstract class Router {
+export abstract class Router extends EventEmitter {
 
     abstract applyRoutes(application: restify.Server);
+
+    public render(response: restify.Response, next: restify.Next) {
+        return (document) => {
+            if (document) {
+                this.emit('beforeRender', document);
+                
+                response.json(document);
+            } else {
+                response.send(404);
+            }
+
+            return next();
+        };
+    }
 
 }
--- users/users.router.ts
+++ users/users.router.ts
@@ -4,35 +4,27 @@ import { User } from './users.model';
 
 class UsersRouter extends Router {
 
+    constructor() {
+        super();
+
+        this.on('beforeRender', document => {
+            document.password = undefined;
+        })
+    }
+
     applyRoutes(application: restify.Server) {
         application.get('/users', (req, res, next) => {
-            User.find().then(users => {
-                res.json(users);
-                return next();
-            })
+            User.find().then(this.render(res, next));
         });
 
         application.get('/users/:id', (req, res, next) => {
-            User.findById(req.params.id).then(user=>{
-                if (user) {
-                    res.json(user);
-                    return next();
-                }
-              
-                res.send(404);
-                return next();
-            });
+            User.findById(req.params.id).then(this.render(res, next));
         });
 
         application.post('/users', (req, res, next) => {
             let user = new User(req.body);
 
-            user.save().then((user: User) => {
-                user.password = undefined;
-
-                res.json(user);
-                return next();
-            })
+            user.save().then(this.render(res, next));
         });
 
         application.put('/users/:id', (req, res, next) => {
@@ -47,25 +39,14 @@ class UsersRouter extends Router {
                     } else {
                         res.send(404);
                     }
-                }).then((user: User) => {
-                    res.json(user);
-                    return next();
-                })
+                }).then(this.render(res, next));
         });
 
         application.patch('/users/:id', (req, res, next) => {
             const options = { new : true };
 
             User.findByIdAndUpdate(req.params.id, req.body, options)
-                .then((user: User) => {
-                    if (user) {
-                        res.json(user);
-                        return next();
-                    }
-
-                    res.send(404);
-                    return next();
-                });
+                .then(this.render(res, next));
         });
 
         application.del('/users/:id', (req, res, next) => {
@@ -82,6 +63,7 @@ class UsersRouter extends Router {
                 });
         });
     };
+
 }
 
 export const usersRouter = new UsersRouter();
